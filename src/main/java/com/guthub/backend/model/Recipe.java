830fa1ff2c471316db2550284b22d5cea085e79a
Duncan Volk <duncanvolk@rogers.com>
2026-02-24 19:59:39 -0500

    recipe.java

--- src/main/java/com/guthub/backend/model/Recipe.java
+++ src/main/java/com/guthub/backend/model/Recipe.java
@@ -1,5 +1,18 @@
 package com.guthub.backend.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Recipe {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //this makes it so ids are automaticall generated, probably what we want
+    private Long id;
+
+    //random stuff idk what we asctually want
+    private String name;
+    private String ingredients;
+    private boolean glutenFree;
+
+    // getters & setters
 }
